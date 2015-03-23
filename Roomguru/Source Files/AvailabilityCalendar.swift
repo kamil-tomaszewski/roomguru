feff0faf5a2d68b108d1ea9a3d06622bbf8b3392
Paweł Białecki <ecler1@gmail.com>
2015-03-23 16:36:34 +0100

    Further implementation of AvailabilityCalendar
    Added tips for future developers. [#88696248]

---
+++ Roomguru/Source Files/AvailabilityCalendar.swift
@@ -0,0 +1,50 @@
+//
+//  FreeBusyCalendar.swift
+//  Roomguru
+//
+//  Created by Pawel Bialecki on 23.03.2015.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+
+class AvailabilityCalendar: NSObject {
+    
+    let id: String
+    let startDate: NSDate
+    let endDate: NSDate
+    var timeFrames: Array<TimeFrame>
+    
+    override var description: String {
+        var customDescription = "calendar id: \(self.id), time frames:\n"
+        
+        for timeFrame in timeFrames {
+            customDescription += "\(timeFrame)\n"
+        }
+        
+        return customDescription
+    }
+    
+    init(id: String, startDate: NSDate, endDate: NSDate, timeFrames: Array<TimeFrame>) {
+        self.id = id
+        self.startDate = startDate
+        self.endDate = endDate
+        self.timeFrames = timeFrames
+        
+        super.init()
+        
+        self.fillWithAvailableTimeFrames()
+    }
+    
+    // sketches for further implementation
+    
+    private func fillWithAvailableTimeFrames() {
+        // self.timeFrames should be filled with *available* TimeFrame instances between not available ones (this will be seen locally only); this would make easy to check (just iterate through array) if we have a free 30 minutes time window to book a room.
+    }
+    
+    func closestFreeTimeFrame() -> TimeFrame? {
+        // returns the closest (in terms of time) free time frame (if we have one - calendar may be filled fully by other bookings)
+        return nil
+    }
+    
+}
