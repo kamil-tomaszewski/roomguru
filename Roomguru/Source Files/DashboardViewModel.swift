3fd9b910713857d2750551b91d46d4f2f25f6827
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-03-27 12:46:20 +0100

    Revert buttons color

--- Roomguru/Source Files/DashboardViewModel.swift
+++ Roomguru/Source Files/DashboardViewModel.swift
@@ -48,7 +48,7 @@ class CellItem {
     init(title: String, action: CellItemAction) {
         self.title = title
         self.action = action
-        _color = (action == .Book) ? UIColor.ngRedColor() : UIColor.ngOrangeColor()
+        _color = (action == .Book) ? UIColor.ngOrangeColor() : UIColor.ngRedColor()
     }
     
     // MARK: Private
