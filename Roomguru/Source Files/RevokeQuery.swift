2abfa87c8a204f367a7621a397a0d648c45be656
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-04-01 10:19:28 +0200

    Revoke query [#88696248]

---
+++ Roomguru/Source Files/RevokeQuery.swift
@@ -0,0 +1,23 @@
+//
+//  RevokeQuery.swift
+//  Roomguru
+//
+//  Created by Radoslaw Szeja on 01/04/15.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class RevokeQuery: Query {
+    
+    convenience init(calendarID: String, eventID: String) {
+        let URLExtension = "/calendars/" + calendarID + "/events/" + eventID
+        self.init(.DELETE, URLExtension: URLExtension)
+    }
+
+    required init(_ HTTPMethod: Alamofire.Method, URLExtension: String, parameters: QueryParameters? = nil, encoding: Alamofire.ParameterEncoding = .URL) {
+        super.init(HTTPMethod, URLExtension: URLExtension, parameters: parameters, encoding: encoding)
+    }
+    
+}
