255da1671a538709a463ce007ecdf7535744c4c8
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-04-07 16:09:35 +0200

    Added chained request [#88696248]

--- Roomguru/Source Files/NetworkManager.swift
+++ Roomguru/Source Files/NetworkManager.swift
@@ -84,7 +84,7 @@ extension NetworkManager {
         }
     }
     
-    func requestList<T where T: ModelJSONProtocol>(query: PageableQuery, success: (response: [T]?) -> (), failure: ErrorBlock) {
+    func requestList<T: ModelJSONProtocol>(query: PageableQuery, success: (response: [T]?) -> (), failure: ErrorBlock) {
         query.setFullPath(serverURL, authKey: key())
         PageableRequest<T>(query).resume(success, failure)
     }
@@ -92,6 +92,44 @@ extension NetworkManager {
     func request(query: Query, success: ResponseBlock, failure: ErrorBlock) {
         query.setFullPath(serverURL, authKey: key())
         QueryRequest(query).resume(success, failure: failure)
+    }
+    
+    /**
+    Executes requests with provided queries. Number of request matches number of queries.
+    Each request result has to conform to ModelJSONProtocol. The final result can be of any type.
+    
+    :param: queries is an array of PageableQuery to be perform one after another, ordered as provided
+    :param: construct a block where results from each request can be processed, or transformed to some other type, and returned
+    :param: success a block invoked if every request was succesful
+    :param: failure a block invoked if error occur
+    */
+    func chainedRequest<T: ModelJSONProtocol, U>(queries: [PageableQuery], construct: (PageableQuery, [T]?) -> [U], success: [U]? -> (), failure: ErrorBlock) {
+        
+        var result: [U] = []
+        var requestError: NSError?
+        
+        let queue: dispatch_queue_t = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        let group: dispatch_group_t = dispatch_group_create();
+        
+        for query in queries {
+            dispatch_group_enter(group)
+            
+            self.requestList(query, success: { (response: [T]?) -> () in
+                result += construct(query, response)
+                dispatch_group_leave(group)
+                
+            }, failure: { (error) -> () in
+                failure(error: error)
+                requestError = error
+                dispatch_group_leave(group)
+            })
+        }
+        
+        dispatch_group_notify(group, queue) {
+            if requestError == nil {
+                success(result)
+            }
+        }
     }
 }
 
