4e2898ed91e69197023d70c99365544e141201c3
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-04-08 16:03:59 +0200

    Added User object and persistance [#92024936]

---
+++ Roomguru/Source Files/User.swift
@@ -0,0 +1,85 @@
+//
+//  User.swift
+//  Roomguru
+//
+//  Created by Radoslaw Szeja on 08/04/15.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+protocol Persistance {
+    
+    class var key: String { get }
+    
+    func save()
+    func delete() -> Bool
+}
+
+class User: NSObject, NSSecureCoding {
+    
+    private(set) var email: String = ""
+    class var current: User? {
+        get {
+            if let data = Defaults[key].data {
+                return NSKeyedUnarchiver.unarchiveObjectWithData(data) as? User
+            }
+            return nil
+        }
+    }
+    
+    init(auth: GTMOAuth2Authentication) {
+        if let email = auth.userEmail {
+            self.email = email
+        }
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        email = aDecoder.decodeObjectForKey("email") as String
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(email, forKey: "email")
+    }
+    
+    class func supportsSecureCoding() -> Bool {
+        return true
+    }
+}
+
+// MARK: Private
+
+extension User {
+    
+    private func googlePlusService(auth: GTMOAuth2Authentication) -> GTLServicePlus {
+        let service = GTLServicePlus()
+        service.retryEnabled = true
+        service.authorizer = auth
+        service.apiVersion = "v1"
+        return service
+    }
+}
+
+
+// MARK: Persistance
+
+extension User: Persistance {
+    
+    class var key: String { get { return "logged_user" } }
+    
+    func save() {
+        Defaults[User.key] = NSKeyedArchiver.archivedDataWithRootObject(self)
+        Defaults.synchronize()
+    }
+    
+    func delete() -> Bool {
+        if Defaults.hasKey(User.key) {
+            Defaults.remove(User.key)
+            Defaults.synchronize()
+            return true
+        }
+        return false
+    }
+    
+}
