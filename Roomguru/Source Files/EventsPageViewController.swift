9378cf753932cd3ff9bf2deddc97a325df9f36e4
Radosław Szeja <radoslaw.szeja@netguru.co>
2015-04-10 12:50:07 +0200

    Removed unnecessary file [#92115022]

--- Roomguru/Source Files/EventsPageViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EventsPageViewController.swift
-//  Roomguru
-//
-//  Created by Radoslaw Szeja on 10/04/15.
-//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
-//
-
-import Foundation
