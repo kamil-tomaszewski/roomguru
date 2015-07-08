8a016807ac5a33ff5bdf7d01d9854e5571938627
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-07-09 00:58:48 +0200

    Finished EditEventNetworkCooperator tests [#96083240]

--- Unit Tests/EditEventNetworkCooperatorSpec.swift
+++ Unit Tests/EditEventNetworkCooperatorSpec.swift
@@ -36,27 +36,110 @@ class EditEventNetworkCooperatorSpec: QuickSpec {
         
         describe("when saving event") {
             
-            pending("and server responds") {
+            context("and server responds") {
                 
                 class MockNetworkManager: NetworkManager {
+                    
+                    let shoulReturnBusyEventInProvidedHours: Bool
+                    
+                    init(shoulReturnBusyEventInProvidedHours: Bool) {
+                        self.shoulReturnBusyEventInProvidedHours = shoulReturnBusyEventInProvidedHours
+                        super.init()
+                    }
+                    
                     override func request(query: Query, success: ResponseBlock, failure: ErrorBlock) {
-                        
+
+                        /* NOTICE:
+                         * Becuase saveEvent() method of sut calls request method of NetworkManager twice (private checkEventsAvailability() and save() ),
+                         * it's needed to distinguish responses by query type
+                         */
                         if query is EventQuery {
+                            let response = [
+                                "id" : "Fixture Event ID",
+                                "start" : ["dateTime" : "2015-04-24T01:00:00-07:00", "timeZone" : "Europe/Warsaw"],
+                                "end" : ["dateTime" : "2015-04-24T01:01:00-07:00", "timeZone" : "Europe/Warsaw"],
+                                "attendees" : [
+                                    [
+                                        "email" : "Fixture Organizer Email Address",
+                                        "responseStatus" : "accepted",
+                                        "displayName" : "Fixture Organizer Display Name",
+                                        "organizer" : "true"
+                                    ],[
+                                        "email" : "Fixture Resource ID",
+                                        "displayName" : "Fixture Resource Display Name",
+                                        "responseStatus" : "accepted",
+                                        "resource" : "true"
+                                    ]
+                                ],
+                                "organizer" : [
+                                    "email" : "Fixture Organizer Email Address",
+                                    "self" : "true",
+                                    "displayName" : "Fixture Organizer Display Name"
+                                ]
+                            ]
                             
+                            success(response: JSON(response))
+                        
                         } else if query is FreeBusyQuery {
                             
+                            let busy: [String : AnyObject] = shoulReturnBusyEventInProvidedHours ?
+                                ["start" : "2015-04-24T00:22:00+02:00", "end" : "2015-04-24T10:30:00+02:00"] :
+                                ["start" : "2015-04-24T00:00:00+02:00", "end" : "2015-04-24T10:30:00+02:00"]
+                            
+                            
+                            let response = [
+                                "timeMin" : "2015-04-23T22:00:00.000Z",
+                                "timeMax" : "2015-04-25T22:00:00.000Z",
+                                "kind" : "calendar#Fixture Calendar Type",
+                                "calendars" : [
+                                    "Fixture Calendar ID" : [
+                                        "busy" : busy
+                                    ]
+                                ]
+                            ]
+                            
+                            success(response: JSON(response))
                         }
                     }
                 }
                 
-                pending("and event is busy at provided hours") {
+                context("and event is busy at provided hours") {
                     
+                    beforeEach {
+                        sut = EditEventNetworkCooperator(usingMockNetworkManager: MockNetworkManager(shoulReturnBusyEventInProvidedHours: true), query: self.mockEventQuery())
+                        sut.saveEvent() { (event, error) in
+                            anEvent = event
+                            anError = error
+                        }
+                    }
+                    
+                    it("error should be nil") {
+                        expect(anError).toNot(beNil())
+                    }
+                    
+                    it("event should be nil") {
+                        expect(anEvent).to(beNil())
+                    }
                 }
                 
-                pending("and event is free at provided hours") {
+                context("and event is free at provided hours") {
                     
+                    beforeEach {
+                        sut = EditEventNetworkCooperator(usingMockNetworkManager: MockNetworkManager(shoulReturnBusyEventInProvidedHours: false), query: self.mockEventQuery())
+                        sut.saveEvent() { (event, error) in
+                            anEvent = event
+                            anError = error
+                        }
+                    }
+                    
+                    it("error should not be nil") {
+                        expect(anError).toNot(beNil())
+                    }
+                    
+                    it("event should be nil") {
+                        expect(anEvent).to(beNil())
+                    }
                 }
-                
             }
             
             describe("and an error did appear") {
